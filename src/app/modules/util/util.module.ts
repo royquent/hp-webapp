bc60b4158492b3a63a4b8b569d9702cfd9ad6601
royquent <quentin.roy.fr@gmail.com>
2023-03-12 13:05:32 +0100

    feat: add capitalize first letter pipe

--- src/app/modules/util/util.module.ts
+++ src/app/modules/util/util.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
 import { RouterModule } from '@angular/router';
+import { CapitalizeFirstLetterPipe } from 'src/app/pipes/capitalize-first-letter.pipe';
 
 
 
 @NgModule({
   exports:[
-    BreadcrumbComponent
+    BreadcrumbComponent,
+    CapitalizeFirstLetterPipe
   ],
   declarations: [
-    BreadcrumbComponent
+    BreadcrumbComponent,
+    CapitalizeFirstLetterPipe
   ],
   imports: [
     RouterModule,
